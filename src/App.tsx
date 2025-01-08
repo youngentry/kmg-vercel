5dd6c51a369811f1c478a633760cc0a527b37e71
yh <gentry_@naver.com>
2025-01-08 17:56:52 +0900

    feat: remove request api

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,47 @@
-import MainPage from "./components/pages/MainPage";
-import { Routes, Route } from "react-router-dom";
-import AttachmentPage from "./components/pages/AttachmentPage";
-import DashboardPage from "./components/pages/DashboardPage";
-import Footer from "./components/sections/footer/Footer";
-import DetailPage from "./components/pages/DetailPage";
-import FloatingMenu from "./components/molecules/common/FloatingMenu";
-import Wrapper from "./components/wrapper/Wrapper";
-import GlobalStyle from "./style/GlobalStyles";
-import Navigation from "./components/sections/navigation/Navigation";
-import { useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { getTokenFromCookie } from "./module/common/getTokenFromCookie";
-import PostPage from "./components/pages/PostPage";
-import LogInForm from "./components/organisms/login/LogInForm";
-import { UserData } from "./@types/index.d";
-import UserPage from "./components/pages/UserPage";
-import SignUpForm from "./components/organisms/login/SignUpForm";
-import DemoPage from "./components/pages/DemoPage";
-import NotFoundPage from "./components/pages/NotFoundPage";
+import MainPage from './components/pages/MainPage';
+import { Routes, Route } from 'react-router-dom';
+import AttachmentPage from './components/pages/AttachmentPage';
+import DashboardPage from './components/pages/DashboardPage';
+import Footer from './components/sections/footer/Footer';
+import DetailPage from './components/pages/DetailPage';
+import FloatingMenu from './components/molecules/common/FloatingMenu';
+import Wrapper from './components/wrapper/Wrapper';
+import GlobalStyle from './style/GlobalStyles';
+import Navigation from './components/sections/navigation/Navigation';
+import { useLocation } from 'react-router-dom';
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+// import { getTokenFromCookie } from "./module/common/getTokenFromCookie";
+import PostPage from './components/pages/PostPage';
+import LogInForm from './components/organisms/login/LogInForm';
+// import { UserData } from "./@types/index.d";
+import UserPage from './components/pages/UserPage';
+import SignUpForm from './components/organisms/login/SignUpForm';
+import DemoPage from './components/pages/DemoPage';
+import NotFoundPage from './components/pages/NotFoundPage';
 
 function App() {
   const location = useLocation();
-  const isDashboardPage = location.pathname === "/dashboard";
+  const isDashboardPage = location.pathname === '/dashboard';
 
-  const [userData, setUserData] = useState<UserData | null>(null);
-  const [accessToken, setAccessToken] = useState<string>("");
+  // const [userData, setUserData] = useState<UserData | null>(null);
+  // const [accessToken, setAccessToken] = useState<string>("");
 
-  useEffect(() => {
-    const cookieCheckForRememberLogin = async () => {
-      const cookieAccessToken = getTokenFromCookie(document.cookie);
-      if (cookieAccessToken) {
-        const result = await axios.post("/api/users/login", null, {
-          headers: {
-            Authorization: `Bearer ${cookieAccessToken}`,
-          },
-        });
-        setAccessToken(accessToken);
-        return setUserData(result.data.data);
-      }
-    };
-    (async () => cookieCheckForRememberLogin())();
-  }, []);
+  // useEffect(() => {
+  //   const cookieCheckForRememberLogin = async () => {
+  //     const cookieAccessToken = getTokenFromCookie(document.cookie);
+  //     if (cookieAccessToken) {
+  //       const result = await axios.post("/api/users/login", null, {
+  //         headers: {
+  //           Authorization: `Bearer ${cookieAccessToken}`,
+  //         },
+  //       });
+  //       setAccessToken(accessToken);
+  //       return setUserData(result.data.data);
+  //     }
+  //   };
+  //   (async () => cookieCheckForRememberLogin())();
+  // }, []);
 
   return (
     <>
@@ -52,12 +52,12 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/attachment" element={<AttachmentPage />} />
-          <Route>
+          {/* <Route>
             <Route path="/users" element={<UserPage />}>
               <Route path="login" element={<LogInForm />} />
               <Route path="create" element={<SignUpForm />} />
             </Route>
-          </Route>
+          </Route> */}
           <Route path="/posts" element={<PostPage />} />
           <Route path="/demo" element={<DemoPage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
--- src/components/molecules/navigation/NavMenus.tsx
+++ src/components/molecules/navigation/NavMenus.tsx
@@ -1,10 +1,10 @@
-import React, { memo } from "react";
-import { FlexCenterDiv } from "../../atoms/FlexDiv";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-import LogOutButton from "../../organisms/login/LogOutButton";
-import { useSelector } from "react-redux";
-import { UserData } from "../../../@types/index.d";
+import React, { memo } from 'react';
+import { FlexCenterDiv } from '../../atoms/FlexDiv';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+// import LogOutButton from '../../organisms/login/LogOutButton';
+// import { useSelector } from 'react-redux';
+// import { UserData } from '../../../@types/index.d';
 
 const MenuBox = styled.div`
   display: flex;
@@ -23,18 +23,22 @@ const Menus = styled(FlexCenterDiv)`
   gap: 4rem;
 `;
 
-const NavMenus = ({ isAnalyzedMessagesExist }: { isAnalyzedMessagesExist: boolean }) => {
-  const userData = useSelector((state: { userLoginDataSlice: UserData }) => state.userLoginDataSlice);
+const NavMenus = ({
+  isAnalyzedMessagesExist,
+}: {
+  isAnalyzedMessagesExist: boolean;
+}) => {
+  // const userData = useSelector((state: { userLoginDataSlice: UserData }) => state.userLoginDataSlice);
 
   return (
     <MenuBox>
       <Menus>
         {!isAnalyzedMessagesExist && <Link to="/demo">미리보기</Link>}
-        <Link to="/attachment">분석하기</Link>
+        <Link to="/attachment#">분석하기</Link>
         {isAnalyzedMessagesExist && <Link to="/dashboard">대시보드</Link>}
         {isAnalyzedMessagesExist && <Link to="/detail">상세보기</Link>}
-        {userData.userId ? <LogOutButton /> : <Link to="/users/login">로그인</Link>}
-        <Link to="/posts">게시판</Link>
+        {/* {userData.userId ? <LogOutButton /> : <Link to="/users/login">로그인</Link>} */}
+        {/* <Link to="/posts">게시판</Link> */}
       </Menus>
     </MenuBox>
   );
